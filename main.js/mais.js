4e88a97af45b3469e508c2f5e8a9c26a8b218866
aseelsaleh123 <salehaseel129@gmail.com>
2024-06-02 21:30:56 +0300

    first commit

---
+++ main.js/mais.js
@@ -0,0 +1,5 @@
+
+document.querySelector(".userProfile h2").textContent="hello aseel";
+console.log(document.querySelector(".userProfile h2"));
+document.querySelector(".userinfo p").classList.add("para");
+console.log(document.querySelector(".userinfo p"));
